5d36a58d1b8fb99472c7c8a20a012fc50638a0a9
Takeshi Nakatani <nakatani@yahoo-corp.jp>
2023-08-31 12:00:38 +0900

    Updated tool scripts and support OSs

--- .github/workflows/imagetypevars.sh
+++ .github/workflows/imagetypevars.sh
@@ -87,8 +87,8 @@ elif [ "${CI_DOCKER_IMAGE_OSTYPE}" = "alpine" ]; then
 	PKGMGR_INSTALL_OPT="add -q --no-progress --no-cache"
 	PKGMGR_UNINSTALL_OPT="del -q --purge --no-progress --no-cache"
 	PKG_INSTALL_CURL="curl"
-	PKG_INSTALL_BASE="k2htpdtor"
-	PKG_INSTALL_DEV="chmpx-dev k2htpdtor"
+	PKG_INSTALL_BASE="k2htpdtor bind-tools"
+	PKG_INSTALL_DEV="chmpx-dev k2htpdtor bind-tools"
 
 elif [ "${CI_DOCKER_IMAGE_OSTYPE}" = "ubuntu" ]; then
 	PKGMGR_NAME="apt-get"
@@ -96,8 +96,8 @@ elif [ "${CI_DOCKER_IMAGE_OSTYPE}" = "ubuntu" ]; then
 	PKGMGR_INSTALL_OPT="install -qq -y"
 	PKGMGR_UNINSTALL_OPT="purge --auto-remove -q -y"
 	PKG_INSTALL_CURL="curl"
-	PKG_INSTALL_BASE="k2htpdtor"
-	PKG_INSTALL_DEV="chmpx-dev k2htpdtor"
+	PKG_INSTALL_BASE="k2htpdtor dnsutils"
+	PKG_INSTALL_DEV="chmpx-dev k2htpdtor dnsutils"
 	UPDATE_LIBPATH="ldconfig"
 
 	#
